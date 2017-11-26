fa156aea26a6a5d0094a94286fdacaa9c555d749
agnim25 <agnim.agarwal@outlook.com>
2017-11-26 09:17:41 -0500

    EDIT: Active Nav Headings w/ JQuery

--- nav.js
+++ nav.js
@@ -5,20 +5,20 @@ document.write('\
       <a href="index.html" class="brand-logo">&nbsp;AI 101</a>\
       <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>\
       <ul class="right hide-on-med-and-down">\
-        <li class="1n"><a href="index.html">Home</a></li>\
-        <li class="2n"><a href="abot.html">About Us</a></li>\
-        <li class="3n"><a href="uses.html">Uses</a></li>\
-        <li class="4n"><a href="tech.html">Technology</a></li>\
-        <li class="5n"><a href="conc.html">Concerns</a></li>\
+        <li class="home"><a href="index.html">Home</a></li>\
+        <li class="abot"><a href="abot.html">About Us</a></li>\
+        <li class="uses"><a href="uses.html">Uses</a></li>\
+        <li class="tech"><a href="tech.html">Technology</a></li>\
+        <li class="conc"><a href="conc.html">Concerns</a></li>\
       </ul>\
     </div>\
   </nav>\
 </div>\
 <ul class="side-nav" id="mobile-demo">\
-  <li><a href="index.html">Home</a></li>\
-  <li><a href="abot.html">About Us</a></li>\
-  <li><a href="uses.html">Uses</a></li>\
-  <li><a href="tech.html">Technology</a></li>\
-  <li><a href="conc.html">Concerns</a></li>\
+  <li class="home"><a href="index.html">Home</a></li>\
+  <li class="abot"><a href="abot.html">About Us</a></li>\
+  <li class="uses"><a href="uses.html">Uses</a></li>\
+  <li class="tech"><a href="tech.html">Technology</a></li>\
+  <li class="conc"><a href="conc.html">Concerns</a></li>\
 </ul>\
 ');
